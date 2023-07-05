81946f331650804a2adae38f97fa97bf6c19c883
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-07-05 15:25:14 -0400

    Finished Exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Input a number to convert into celsius.");
+            double fTemp = double.Parse(Console.ReadLine());
+            TempConverter.FahrenheitToCelsius(fTemp);
+            Console.WriteLine($"{fTemp} farenheit converts to {TempConverter.FahrenheitToCelsius(fTemp)} celsius");
+
+            Console.WriteLine("Input a number to convert into fahrenheit.");
+            double cTemp = double.Parse(Console.ReadLine());
+            TempConverter.CelsiusToFahrenheit(cTemp);
+            Console.WriteLine($"{cTemp} celsius converts to {TempConverter.CelsiusToFahrenheit(cTemp)} farenheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            //Celsius = (Fahrenheit - 32) * 5/9.
+            double celsius = (fahrenheit - 32)* 5 / 9;
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            //Fahrenheit = (Celsius * 9/5) + 32
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+    }
+}
